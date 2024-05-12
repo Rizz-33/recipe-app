9ad82706e8a24304eba29089120037c06e064a0f
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-12 19:51:58 +0530

    refactor: Views, titles of each docs

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+
+recipes = [
+    {
+        'author': 'Risini',
+        'title': 'Chicken Pie',
+        'direction': 'Add more chicken pieces with cheese',
+        'date_posted': 'May 12 2024'
+    },
+    {
+        'author': 'Dulaj',
+        'title': 'Pizza',
+        'direction': 'Add cheese, pepperoni and other stuff',
+        'date_posted': 'May 11 2024'
+    },
+    {
+        'author': 'Thamara',
+        'title': 'Cheese Cake',
+        'direction': 'Mix cheese and butter',
+        'date_posted': 'May 10 2024'
+    }
+]
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    context = {
+        'recipes': recipes,
+    }
+    return render(request, 'recipes/home.html', context)
 
 def about(request):
-    return render(request, 'recipes/about.html')
+    return render(request, 'recipes/about.html', {'title': 'About Us'})
