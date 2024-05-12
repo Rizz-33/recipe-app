96e77689781d1ea2f86db57637c3bc30a9bcef55
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-12 15:09:15 +0530

    refactor: URLs and views

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Welcome To My Recipes App!</h1>")
