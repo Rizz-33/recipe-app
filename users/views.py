2fd9ad14bcdb4ef0f3b8733e9853d05457dad73e
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-13 11:34:45 +0530

    refactor: Register

--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from . import forms
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"{username}, your account has been created")
             return redirect('recipes-home')
     else:
-        form = UserCreationForm()
+        form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
