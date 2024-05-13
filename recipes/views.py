610c8c8c5caafc29ba5a4f4ab5d0c30f5e151baa
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-13 22:23:50 +0530

    refactor: Recipe view

--- recipes/views.py
+++ recipes/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from . import models
 
-recipes = [
-    {
-        'author': 'Risini',
-        'title': 'Chicken Pie',
-        'direction': 'Add more chicken pieces with cheese',
-        'date_posted': 'May 12 2024'
-    },
-    {
-        'author': 'Dulaj',
-        'title': 'Pizza',
-        'direction': 'Add cheese, pepperoni and other stuff',
-        'date_posted': 'May 11 2024'
-    },
-    {
-        'author': 'Thamara',
-        'title': 'Cheese Cake',
-        'direction': 'Mix cheese and butter',
-        'date_posted': 'May 10 2024'
-    }
-]
 
 def home(request):
     recipes = models.Recipe.objects.all()
