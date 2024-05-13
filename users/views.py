b03c613145cb28120c0407121c0099cdf868993d
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-13 16:56:59 +0530

    feat: Login and Logout

--- users/views.py
+++ users/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods  # Import the decorator
 from . import forms
 
-# Create your views here.
+@login_required
+@require_http_methods(["GET", "POST"])  # Add the decorator
 def register(request):
     if request.method == 'POST':
         form = forms.UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"{username}, your account has been created")
-            return redirect('recipes-home')
+            messages.success(request, f"{username}, your account has been created, please login again")
+            return redirect('user-login')
     else:
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
