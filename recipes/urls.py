66e83245c8592959760e1235c1506d95f5559f1b
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-05-13 23:12:14 +0530

    feat: Update recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.RecipeListView.as_view(), name='recipes-home'),
     path('recipe/create', views.RecipeCreateView.as_view(), name='recipes-create'),
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name='recipes-detail'),
+    path('recipe/<int:pk>/update/', views.RecipeUpdateView.as_view(), name='recipes-update'),
     path('about/', views.about, name='recipes-about'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from . import models
 
@@ -23,6 +23,20 @@ class RecipeDetailView(DetailView):
 class RecipeCreateView(LoginRequiredMixin,CreateView):
     model = models.Recipe
     fields = ['title', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class RecipeUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = models.Recipe
+    fields = ['title', 'description']
+
+    def test_func(self):
+        recipe = self.get_object()
+        if self.request.user == recipe.author:
+            return True
+        return False
 
     def form_valid(self, form):
         form.instance.author = self.request.user
